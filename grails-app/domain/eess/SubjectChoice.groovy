2fac9da4d1a270300cd12bec128576c5a168ecc9
Paul <paul.frunza.hd@gmail.com>
2012-05-03 10:22:53 +0200

    SubjectChoices can now save pools

--- grails-app/domain/eess/SubjectChoice.groovy
+++ grails-app/domain/eess/SubjectChoice.groovy
@@ -1,10 +1,20 @@
 package eess
 
+import java.util.List;
+
 class SubjectChoice {
+	final static List<String> allowedPools=['A', 'B']
+	
 	int priority	//primary or secondary 
+	String pool		//pool a or b or none
 	
 	static belongsTo = [student: Users, subject: Subject]
 
     static constraints = {
+		pool(nullable: true, validator:{val ->
+			if(!SubjectChoice.allowedPools.contains(val)) {
+				return "Invalid pool"
+			}
+		})
     }
 }
